74d278d5e7938a85d7820fca9851a7fde31e20a7
YAAP <yaapis@gmail.com>
2017-07-17 02:41:28 +0300

    base without vuex

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+	<div id="app">
+		<img src="./assets/logo.png">
+		<router-view></router-view>
+		<links></links>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Links from './components/Links.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      Links
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
---
+++ src/components/Links.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul>
+      <li><router-link :to="{ name: 'home'}">Home</router-link></li>
+      <li><router-link :to="{ name: 'contacts'}">Contacts</router-link></li>
+      <li><router-link :to="{ name: 'pages', params: { page: 'about' }}">Pages - about</router-link></li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'Links'
+}
+</script>
